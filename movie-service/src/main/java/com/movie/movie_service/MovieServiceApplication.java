65022c51954de48a8719d0e890099ec2b4c1a0c0
om kharmate <omkharmate85@gmail.com>
2026-05-28 17:02:08 +0530

    feature(movie-service): implement Redis caching for movie APIs

--- movie-service/src/main/java/com/movie/movie_service/MovieServiceApplication.java
+++ movie-service/src/main/java/com/movie/movie_service/MovieServiceApplication.java
@@ -2,8 +2,10 @@ package com.movie.movie_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class MovieServiceApplication {
 
 	public static void main(String[] args) {
